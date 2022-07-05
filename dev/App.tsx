c3f1534db04fec343d1544cb87a9157a84dc6a61
007 <dr007cc@gmail.com>
2022-07-05 23:38:56 +0430

    cleaning the DevApp

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,21 +1,10 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import { render } from 'react-dom'
-
-import { CountAnim } from '..'
 
 import './style.scss'
 
 const App: FC = () => {
-    const [Count, setCount] = useState(1e7)
-
-    return (
-        <div className='app'>
-            <CountAnim end={Count} />
-            <button onClick={() => setCount(s => s + 1)}>
-                Add Count {Count}
-            </button>
-        </div>
-    )
+    return <div className='app'></div>
 }
 
 render(<App />, document.getElementById('root'))
